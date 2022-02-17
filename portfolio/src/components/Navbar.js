432d2a9e783bf82e597eb346769b139b3719562a
JakubStranianek <j.stranianek@gmail.com>
2022-02-17 15:49:41 +0100

    HomeNavbarComponent

---
+++ portfolio/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import '../components/Navbar.css';
+
+function Navbar() {
+   return (
+       <ul>
+           <li><a href="#">Home</a></li>
+           <li><a href="#">About</a></li>
+           <li><a href="#">Services</a></li>
+           <li><a href="#">Portfolio</a></li>
+           <li><a href="#">Contact Me</a></li>
+       </ul>
+   ); 
+}
+
+export default Navbar;
