7aa05b535d9fcc12449d3b663f4da4db7d1122cf
JakubStranianek <j.stranianek@gmail.com>
2022-02-28 15:10:03 +0100

    ProgressBar as component

--- portfolio/src/components/Skills/SkillsCharts.js
+++ portfolio/src/components/Skills/SkillsCharts.js
@@ -1,73 +1,74 @@
 import "./SkillsCharts.css"
+import { CircularProgressBar } from "@tomik23/react-circular-progress-bar";
 
 function SkillsCharts() {
     const charts = [
         {
             id: "1",
             name: "HTML",
-            percentage: "80%",
+            percentage: "80",
             startColor: "#FE8500",
             endColor: "#FE4F00"
         },
         {
             id: "2",
             name: "CSS",
-            percentage: "80%",
+            percentage: "80",
             startColor: "#1E79DE",
             endColor: "#4E32BB"
         },
         {
             id: "3",
             name: "JS",
-            percentage: "50%",
+            percentage: "50",
             startColor: "#FFF301",
             endColor: "#FF9701"
         },
         {
             id: "4",
             name: "DATABASES",
-            percentage: "60%",
+            percentage: "60",
             startColor: "#D7A1CD",
             endColor: "#8441DE"
         },
         {
             id: "5",
             name: "FIGMA",
-            percentage: "50%",
+            percentage: "50",
             startColor: "#40C5A1",
             endColor: "#1D6B9E"
         },
         {
             id: "6",
             name: "ADOBE PS + AI",
-            percentage: "65%",
+            percentage: "65",
             startColor: "#4DDDFE",
             endColor: "#DA4AFC"
         }
     ]
-    
+
     return (
         <div className="chartWrapper">
             {charts.map(index => 
+            <div className="flexGrid">
                 <div className="grid-Item" id={index.id}>
-                    <div className="outerCircle">
-                        <div className="innerCircle">
-                            <h2>{index.percentage}</h2>
-                        </div>
-                    </div>
-                    <h2>{index.name}</h2>
-
-                    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="210px" height="210px" >
-         <defs>
-            <linearGradient id={"GradientColor" + index.id}>
-               <stop offset="0%" stopColor={index.startColor} />
-               <stop offset="100%" stopColor={index.endColor} />
-            </linearGradient>
-         </defs>
-         <circle cx="105" cy="100" r="88" strokeLinecap="round" stroke={"url(#GradientColor" + index.id + ")"}/>
-        </svg>
-
-                </div> 
+                <CircularProgressBar id={index.id}
+                   percent={index.percentage}
+                   fontColor={"white"}
+                   fontSize={"16px"}
+                   colorCircle={"white"}
+                   rotation={0}
+                   round={true}
+                   speed= {1000}
+                   styles={{
+                    borderRadius: '50%',
+                    boxShadow: "4px 4px 6px -1px inset rgba(255,255,255,0.2)"
+                  }}
+                   linearGradient={[index.startColor, index.endColor]}
+                    />
+                </div>
+                <h2>{index.name}</h2>
+            </div>
             )}
         </div>
     );
