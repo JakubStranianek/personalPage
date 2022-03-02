7c15c96190d8f0fef4874fe272cbb812b7b727e4
JakubStranianek <j.stranianek@gmail.com>
2022-03-02 10:14:15 +0100

    logo portfolio completed

--- portfolio/src/components/PortfolioLogo/PortfolioLogoGrid.js
+++ portfolio/src/components/PortfolioLogo/PortfolioLogoGrid.js
@@ -3,6 +3,9 @@ import bufet from "../../assets/bufet.png"
 import barber from "../../assets/villiz.png"
 import coffee from "../../assets/coffee.png"
 import systems from "../../assets/systems.png"
+import vision from "../../assets/vision.png"
+import school from "../../assets/school.png"
+import pohoda from "../../assets/pohoda.png"
 
 function PortfolioLogoGrid() {
     return (
@@ -13,14 +16,19 @@ function PortfolioLogoGrid() {
             <div className="img2">
             <img src={bufet} alt="bufetLogo"></img>
             </div>
-            <div className="img3"></div>
+            <div className="img3">
+            <img src={vision} alt="visionLogo"></img>
+            </div>
             <div className="img4">
             <img src={systems} alt="systemsLogo"></img>    
             </div>
             <div className="img5">
             <img src={coffee} alt="coffeeLogo"></img>
             </div>
-            <div className="img6"></div>
+            <div className="img6">
+            <img src={school} alt="schoolLogo"></img>
+            <img src={pohoda} alt="pohodaLogo"></img>
+            </div>
         </div>
     );
 }
