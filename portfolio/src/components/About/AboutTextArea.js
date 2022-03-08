714f1e528649b515a9d2c5d21e9b59e5e75f304d
JakubStranianek <j.stranianek@gmail.com>
2022-03-08 10:34:54 +0100

    little changes on hover

--- portfolio/src/components/About/AboutTextArea.js
+++ portfolio/src/components/About/AboutTextArea.js
@@ -1,13 +1,21 @@
 import "./AboutTextArea.css"
+import { saveAs } from "file-saver";
 
 function AboutTextArea() {
+    const saveFile = () => {
+        saveAs(
+          "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf",
+          "StranianekCV.pdf"
+        );
+      }
+    
     return (
         <div className="ml30">
             <p>Who I am</p>
             <h1>About Me</h1>
             <article>
             Hi there. My name is Jakub and im an ex computer science 
-            student from Zilina university. In my free time im focusing 
+            student from Žilina university. In my free time im focusing 
             my time on graphic design in a form of pictures or other forms such as logos , billboards etc.
             </article>
             
@@ -17,7 +25,7 @@ function AboutTextArea() {
             film and spending time with those who i care about. 
             </article>
 
-            <button className="btnCV">DOWNLOAD CV</button>
+            <button className="btnCV" onClick={saveFile}>DOWNLOAD CV</button>
         </div>
     );
 }
